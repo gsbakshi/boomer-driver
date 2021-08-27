9f25b236eab78f4a3f5c6d6797654b924e2a9c16
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-08-27 05:33:13 +0530

    Updating Dependencies

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:driver/main.dart';
+import 'package:boomer_driver/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
