4497bd030e48be036bd3da287c24f6e6d7f72950
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-08-30 17:03:34 +0530

    Fixed map loading bug and termination of location stream

--- lib/providers/maps_provider.dart
+++ lib/providers/maps_provider.dart
@@ -5,40 +5,87 @@ import 'package:geolocator/geolocator.dart';
 import 'package:flutter_geofire/flutter_geofire.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-import 'auth.dart';
+import 'driver_provider.dart';
 
 class MapsProvider with ChangeNotifier {
-  void update(Auth auth) {
-    authToken = auth.token;
-    driverId = auth.driverId;
+  void update(DriverProvider driver) {
+    driverId = driver.driverId;
+    status = driver.status;
   }
 
-  late String? authToken;
   late String? driverId;
+
+  late bool status;
 
   late Position _currentPosition;
 
   Position get currentPosition => _currentPosition;
 
-  late StreamSubscription<Position> liveLocationStream;
+  late GoogleMapController _newMapController;
 
-  Future<void> locatePosition(GoogleMapController mapController) async {
+  GoogleMapController get newMapController => _newMapController;
+
+  Future<void> setMapController(GoogleMapController controller) async {
     try {
+      Completer<GoogleMapController> _controller = Completer();
+      _controller.complete(controller);
+      _newMapController = await _controller.future;
+      notifyListeners();
+      await locatePosition();
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  // ignore: cancel_subscriptions
+  StreamSubscription<Position>? liveLocationStream;
+
+  bool isPermissionsInit = true;
+
+  Future<bool> checkPermissions() async {
+    LocationPermission permission;
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied ||
+          permission == LocationPermission.deniedForever) {
+        return false;
+      }
+      isPermissionsInit = false;
+      notifyListeners();
+      return true;
+    } else {
+      isPermissionsInit = false;
+      notifyListeners();
+      return true;
+    }
+  }
+
+  Future<void> geolocate() async {
+    final check = await checkPermissions();
+    if (check) {
       Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high,
       );
-
       _currentPosition = position;
+    }
+    notifyListeners();
+  }
 
-      LatLng latLngPosition = LatLng(position.latitude, position.longitude);
+  Future<void> locatePosition() async {
+    try {
+      await geolocate();
+      LatLng latLngPosition =
+          LatLng(currentPosition.latitude, currentPosition.longitude);
       CameraPosition cameraPosition = new CameraPosition(
         target: latLngPosition,
         zoom: 14,
       );
-
-      mapController.animateCamera(
+      _newMapController.animateCamera(
         CameraUpdate.newCameraPosition(cameraPosition),
       );
+      await goOnline();
+      await getLiveLocationUpdates();
     } catch (error) {
       print(error);
       throw error;
@@ -48,7 +95,8 @@ class MapsProvider with ChangeNotifier {
   Future<void> goOffline() async {
     try {
       await Geofire.removeLocation(driverId!);
-      liveLocationStream.cancel();
+      // _controller = null;
+      // liveLocationStream = null;
     } catch (error) {
       print(error);
       throw error;
@@ -57,10 +105,7 @@ class MapsProvider with ChangeNotifier {
 
   Future<void> goOnline() async {
     try {
-      Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high,
-      );
-      _currentPosition = position;
+      await geolocate();
       await Geofire.initialize('boomer/available-drivers');
       await Geofire.setLocation(
         driverId!,
@@ -73,15 +118,16 @@ class MapsProvider with ChangeNotifier {
     }
   }
 
-  Future<void> getLiveLocationUpdates(
-      GoogleMapController mapController) async {
+  Future<void> getLiveLocationUpdates() async {
     liveLocationStream = Geolocator.getPositionStream().listen(
       (Position position) {
         _currentPosition = position;
+        if (status)
           Geofire.setLocation(driverId!, position.latitude, position.longitude);
         LatLng latLng = LatLng(position.latitude, position.longitude);
-        mapController.animateCamera(CameraUpdate.newLatLng(latLng));
+        _newMapController.animateCamera(CameraUpdate.newLatLng(latLng));
       },
     );
+    notifyListeners();
   }
 }
